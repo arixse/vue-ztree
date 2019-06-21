ac2e87cf9fcd7587b3ad4cf96bf6ed40de2a19ea
fusheng <fusheng@netposa.com>
2019-06-21 18:46:55 +0800

    fix

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,9 @@
 module.exports = {
 	title: "ztree-for-vue",
+	themeConfig: {
+		nav: [{ text: "指南", link: "/" }, { text: "Api", link: "/api" }],
+		sidebar:false
+	},
 	description: "A Vue.2x component of ztree 👍",
 	configureWebpack: {
 		resolve: {
--- docs/README.md
+++ docs/README.md
@@ -1,10 +1,7 @@
-# ztree-for-vue
-
 ## install
+
 ```sh
 yarn add ztree-for-vue
 ```
-
-
 
 <demo-1></demo-1>
