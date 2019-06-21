810f19437f406ed37a9108044962e4cbab13213c
fusheng <fusheng@netposa.com>
2019-06-21 19:01:01 +0800

    deploy

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,9 @@
 module.exports = {
 	title: "ztree-for-vue",
+	base: "/<REPO>/",
 	themeConfig: {
 		nav: [{ text: "指南", link: "/" }, { text: "Api", link: "/api" }],
-		sidebar:false
+		sidebar: false
 	},
 	description: "A Vue.2x component of ztree 👍",
 	configureWebpack: {
